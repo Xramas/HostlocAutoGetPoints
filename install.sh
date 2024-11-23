c02581b447b9f6b7c68d427a876fadf3fec967b2
Xramas <mastercvmo@gmail.com>
2024-11-23 16:14:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ elif [[ x"${release}" == x"arch" ]]; then
 fi
 
 # 安装 Python 依赖
-python3 -m pip install --upgrade requests pyaes
+python3 -m pip install --upgrade requests pyaes pyyaml
 
 # 克隆 GitHub 仓库
 git clone https://github.com/Xramas/HostlocAutoGetPoints
@@ -68,19 +68,32 @@ clear
 
 # 进入仓库并修改配置
 cd HostlocAutoGetPoints
-config_file=HostlocAutoGetPoints.py
+
+# 创建配置文件 config.yaml
+cat > config.yaml << EOF
+usernames: "your_username"  # 输入多个账号以逗号分隔
+passwords: "your_password"  # 输入多个密码以逗号分隔
+bot_api: "your_bot_api"  # 你的Telegram Bot API
+chat_id: "your_chat_id"  # 你的Telegram Chat ID
+max_retries: 3  # 最大重试次数
+retry_delay: 5  # 登录重试间隔（秒）
+EOF
+
+# 获取用户输入
 printf "请输入Hostloc账号，如果您有多个账号，请使用英文逗号隔开："
-read -r USERNAME <&1
-sed -i "s/USERNAME/$USERNAME/" $config_file
+read -r USERNAME
 printf "请输入Hostloc密码，如果您有多个账号，请使用英文逗号隔开："
-read -r PASSWORD <&1
-sed -i "s/PASSWORD/$PASSWORD/" $config_file
+read -r PASSWORD
 printf "请输入BOT_API，在@BotFather处申请："
-read -r BOT_API <&1
-sed -i "s/BOT_API/$BOT_API/" $config_file
+read -r BOT_API
 printf "请输入CHAT_ID，在@userinfobot处获取："
-read -r CHAT_ID <&1
-sed -i "s/CHAT_ID/$CHAT_ID/" $config_file
+read -r CHAT_ID
+
+# 更新配置文件
+sed -i "s/your_username/$USERNAME/" config.yaml
+sed -i "s/your_password/$PASSWORD/" config.yaml
+sed -i "s/your_bot_api/$BOT_API/" config.yaml
+sed -i "s/your_chat_id/$CHAT_ID/" config.yaml
 
 clear
 echo "安装已完成"
